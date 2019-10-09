4b45c1ad09d073c8bca32559bdf22bf3104b580f
spiderxm <mrigank.rkee@rediffmail.com>
2019-10-10 00:55:00 +0530

    second commit

--- app/src/main/java/com/example/order/MainActivity.java
+++ app/src/main/java/com/example/order/MainActivity.java
@@ -16,6 +16,10 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
     int quantity=2;
+    int quantity1=2;
+    int quantity2=0;
+    int quantity3=0;
+
     boolean w;
     boolean c;
     int price;
@@ -24,7 +28,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void increment1(View view){
+        if(quantity1==100)
+        {
+            Toast.makeText(this ,"You cant order more than 100 cups of Iced Tea",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity1=quantity1+1;
+        display1(quantity1);
 
+    }
+    public void increment2(View view){
+        if(quantity2==quantity)
+        {
+            Toast.makeText(this ,"You cant order more toppings than cups of coffee",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity2=quantity2+1;
+        display2(quantity2);
+
+    }
+    public void increment3(View view){
+        if(quantity3==quantity)
+        {
+            Toast.makeText(this ,"You cant order more toppings than cups of coffee",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity3=quantity3+1;
+        display3(quantity3);
+
+    }
     public void increment(View view){
      if(quantity==100)
      {
@@ -44,30 +77,59 @@ public class MainActivity extends AppCompatActivity {
         w=c1.isChecked();
     }
     public void decrement(View view){
-        if(quantity==1)
+        if((quantity+quantity1)==1)
         {
-            Toast.makeText(this ,"You cant order less than 1 cup of coffee",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this ,"You have to order something",Toast.LENGTH_SHORT).show();
             return;
         }
-        quantity=quantity-1;
+        if(quantity>0)
+        {quantity=quantity-1;
         display(quantity);
+    }}
+    public void decrement2(View view){
+        if(quantity2==0)
+        {
+            Toast.makeText(this ,"You cant order less than 0 topping",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity2=quantity2-1;
+        display2(quantity2);
+    }
+    public void decrement3(View view){
+        if(quantity3==0)
+        {
+            Toast.makeText(this ,"You cant order less than 0 toppings",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity3=quantity3-1;
+        display3(quantity3);
+    }
+    public void decrement1(View view){
+        if((quantity1+quantity)==1)
+        {
+            Toast.makeText(this ,"You have to order something",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(quantity1>0){
+        quantity1=quantity1-1;
+        display1(quantity1);}
     }
     public void submitOrder(View view) {
      if(w==true&&c==false)
      {
-    price=(quantity*5)+(quantity*4);
+      price=(quantity*5)+(quantity2*4)+(quantity1*3);
      }
         if(w==false&&c==true)
         {
-            price=(quantity*5)+(quantity*6);
+            price=(quantity*5)+(quantity3*6)+(quantity1*3);
         }
         if(w==true&&c==true)
         {
-            price=(quantity*5)+(quantity*10);
+            price=(quantity*5)+(quantity2*4)+(quantity3*6)+(quantity1*3);
         }
         if(w==false&&c==false)
         {
-            price=(quantity*5);
+            price=(quantity*5)+(quantity1*3);
         }
         /*this edittext command is for entering the name */
         EditText edittexr=(EditText)findViewById(R.id.edittxt);
@@ -78,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
         Editable nameEditable2=edittxts.getText();
         String name2=nameEditable2.toString();
 String summary="My order summary is";
-String nam="Customer's name "+name+"\nDid you opted for Whipped cream "+w+"\nDid you opt for chocolate "+c+"\nAmount Due  $"+price+"\nSpecial suggestions by the customer are "+name2;
+String nam="Customer's name "+name+"\nNo. of cups of coffee you ordered is "+quantity+"\nNo. of cups of Iced Tea ordered is "+quantity1+"\nDid you opted for Whipped cream "+w+"\nDid you opt for chocolate "+c+"\nAmount Due  $"+price+"\nSpecial suggestions by the customer are "+name2;
 displayMessage(nam);
 
         Intent intent=new Intent(Intent.ACTION_SENDTO);
@@ -96,6 +158,17 @@ displayMessage(nam);
     private void display(int number){
         TextView t1=(TextView)findViewById(R.id.textView2);
         t1.setText(" "+number);
+    }
+    private void display2(int number){
+        TextView t3=(TextView)findViewById(R.id.textView10);
+        t3.setText(" "+number);
+    } private void display3(int number){
+        TextView t4=(TextView)findViewById(R.id.textView11);
+        t4.setText(" "+number);
+    }
+    private void display1(int number){
+        TextView t2=(TextView)findViewById(R.id.textView9);
+        t2.setText(" "+number);
     }
     private void displayPrice(int number){
         TextView t2=(TextView)findViewById(R.id.textView4);
